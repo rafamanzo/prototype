01dca0e3c5adc66e6f51f874f1986157602ba041
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2012-04-15 21:07:22 -0300

    Application skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prototye::Application.config.secret_token = 'd54453b28522307ce58420295095141472814e49d384bfb2f565ac171d11d018db67c0e92bf9474e52b7608e7058fb04608cb6de77de7501fbd34ee184506f4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Prototye::Application.config.session_store :cookie_store, key: '_prototye_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Prototye::Application.config.session_store :active_record_store
